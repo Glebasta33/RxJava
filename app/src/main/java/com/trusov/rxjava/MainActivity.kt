96e3ea0ffc39af0aa4a616b64aa574906ee8a06a
Glebasta333 <gleb.trusow@yandex.ru>
2022-06-08 13:51:20 +0300

    add dispose

--- app/src/main/java/com/trusov/rxjava/MainActivity.kt
+++ app/src/main/java/com/trusov/rxjava/MainActivity.kt
@@ -2,15 +2,20 @@ package com.trusov.rxjava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.*
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivityTag"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +26,22 @@ class MainActivity : AppCompatActivity() {
             Log.e(TAG, "click click")
         }
 
-        Observable.just("Hello RxJava")
-            .map { "$it, my name is" }
-            .map { "$it Gleb" }
-            .subscribe { Toast.makeText(application, it, Toast.LENGTH_SHORT).show() }
+        val disposable: Disposable = Observable.create<Int> { subscriber ->
+            listOf(1, 2, 3, 4, 5).forEach {
+                Thread.sleep(1000)
+                subscriber.onNext(it)
+            }
+        }
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                Log.d(TAG, "value is $it")
+            }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            disposable.dispose()
+            Log.d(TAG, "Disposed")
+        }, 3000)
     }
 
 
