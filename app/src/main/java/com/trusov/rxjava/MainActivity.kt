7b9553505be4a49ae2bb06e5dd62be0af2904976
Glebasta333 <gleb.trusow@yandex.ru>
2022-06-08 12:05:45 +0300

    add Flowable

--- app/src/main/java/com/trusov/rxjava/MainActivity.kt
+++ app/src/main/java/com/trusov/rxjava/MainActivity.kt
@@ -3,28 +3,41 @@ package com.trusov.rxjava
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import io.reactivex.rxjava3.core.Flowable
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
+import android.widget.Button
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.*
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = MainActivity::class.java.simpleName
+    private val TAG = "MainActivityTag"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+            button.setOnClickListener {
+            Log.e(TAG, "click click")
+        }
+
+        val disposable = dataSource()
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                button.text = "click-$it"
+                Log.e(TAG, "next int $it")
+            }, {
+                Log.e(TAG, "Throwable: ${it.localizedMessage}")
+            })
     }
 
-    val observable = Observable.just(1,2,3)
-
-    val single = Single.just(1)
-
-    val flowable = Flowable.just(1,2,3)
-
-    val disposable = flowable.subscribe ({
-        Log.d(TAG, "new data $it")
-    }, {
-
-    })
+    private fun dataSource(): Flowable<Int> {
+        return Flowable.create ({ subscriber ->
+            for (i in 0..500000) {
+                subscriber.onNext(i)
+            }
+            subscriber.onComplete()
+        }, BackpressureStrategy.DROP)
+    }
 
 }
